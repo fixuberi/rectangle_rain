52a4bbb7b46f3147bbf193c497266552e4d5ecb0
fixuberi <comeclarity007@gmail.com>
2019-01-26 05:20:54 +0200

    added Rectangle and RectangleCollection classes for content control purposes

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 class Canvas {
   constructor(canvas, content) {
-    this.width = canvas.clientWidth;
-    this.height = canvas.clientHeight;
-    this.ctx = canvas.getContext('2d');
-    this.gameOn = false;
-    this.content = {
-      rectCollection: content.rectCollection
-    }
+    this.width   = canvas.clientWidth;
+    this.height  = canvas.clientHeight;
+    this.ctx     = canvas.getContext('2d');
+    this.gameOn  = false;
+    this.content = content;
   }
   animateStart() {
     this.gameOn = true;
@@ -18,7 +16,7 @@ class Canvas {
   _animate() {
     this._clear();
     this._fillRectCollection(this.content.rectCollection);
-    this.content.rectCollection.setNextFrame()
+    this.content.setNextFrame()
     if(this.gameOn) {
       requestAnimationFrame(this._animate.bind(this));
      } else this._clear();
@@ -34,24 +32,45 @@ class Canvas {
   }
 }
 
-var canvasEl = document.getElementById('canvas');
-var content = {
-  rectCollection: [
-                    {
-                      posX: 320, 
-                      posY: 0, 
-                      size: [20, 20],
-                      color: '#000000',
-                      stepY: 1
-                    }
-                  ]
-              };
-content.rectCollection.setNextFrame = function() {
-  this.forEach(el => {
-    el.posY += el.stepY;
-    if(el.posY >= canvas.height) el.posY = 0;
-  })
+class RectangleCollection {
+  constructor(canvasHeight) {
+    this.rectCollection = [];
+    this.canvasHeight   = canvasHeight
+  }
+  setNextFrame() {
+    this.rectCollection.forEach(el => el.increaseY(this.canvasHeight));
+  }
+  add(rect) {
+    this.rectCollection.push(rect);
+  }
+  remove(rect) {
+    this.rectCollection = this.rectCollection.filter(el => el !== rect);
+  }
 }
+
+class Rectangle {
+  constructor({ posX, posY, stepY, color, size }) {
+    this.posX  = posX;
+    this.posY  = posY;
+    this.stepY = stepY;
+    this.color = color;
+    this.size  = size;
+  }
+  increaseY(canvasHeight) {
+    this.posY += this.stepY;
+    if (this.posY >= canvasHeight) this.posY = 0;
+  }
+}
+
+var canvasEl = document.getElementById('canvas');
+var content = new RectangleCollection(canvasEl.height);
+content.add(new Rectangle(
+                          { posX: 320,
+                            posY: 0, 
+                            stepY: 1, 
+                            color: '#000000', 
+                            size: [20, 20] }
+                          ));
 var canvas = new Canvas(canvasEl, content);
 document.getElementById('start').addEventListener('click', ()=> {
   canvas.animateStart()
